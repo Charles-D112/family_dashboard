e07a2bcf1b102fe4d3fbeb7eaeabfea9c1ac39a9
Dimitri Kacimi Zlotnik <dimitri.zlotnik@gmail.com>
2023-09-06 10:50:44 +0200

    First push lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,42 @@
 class ListsController < ApplicationController
-  def trophee
+  def index
+    @list = current_user.lists
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+    @list.tasks.build
+  end
+
+  def create
+    @list = current_user.lists.build(list_params)
+    if @list.save
+      redirect_to lists_path, notice: "#{@list.name} ajoutée à vos To Do List!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path, notice: "#{@list.name} mise à jour avec succès!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, tasks_attributes: %i[id name])
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,12 @@
 class List < ApplicationRecord
   belongs_to :user
   has_many :tasks
+
+  validate :one_task_minimum, on: :update
+
+  private
+
+  def one_task_minimum
+    errors.add(:base, "Ajoutez une tâche au minimum pour valider!") if tasks.empty?
+  end
 end
