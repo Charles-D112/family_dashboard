b01912779fe5b3c0765798d8a418c852d08b9da6
Alexandre Bailly <bobbycit2@gmail.com>
2023-09-07 12:39:44 +0200

    footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @show_footer = false # Vous pouvez définir cette variable selon vos besoins initiaux
   end
 end
