d0166859d81fd919696711510f7e5f66529ab81b
Alexandre Bailly <bobbycit2@gmail.com>
2023-09-11 16:58:57 +0200

    push

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toggle"
+export default class extends Controller {
+  connect() {
+    const taskToggles = this.element.querySelectorAll(".task-toggle");
+
+    taskToggles.forEach((toggle) => {
+      toggle.addEventListener("change", () => {
+        const taskId = toggle.dataset.taskId;
+        const taskElement = this.element.querySelector(`[data-task-id="${taskId}"]`);
+
+        if (taskElement) {
+          if (toggle.checked) {
+            taskElement.style.display = "none";
+          } else {
+            taskElement.style.display = "block";
+          }
+        }
+      });
+    });
+  }
+}
