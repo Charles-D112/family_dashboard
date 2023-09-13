8c2ca167b4b914df90082c79e24031f33d96e58a
Dimitri Kacimi Zlotnik <dimitri.zlotnik@gmail.com>
2023-09-13 14:51:00 +0200

    Push mid heroku back quasi over

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :trophees
   resources :tutos
-<<<<<<< HEAD
-  resources :lists, only: %i[index show new create edit update destroy] do
-=======
   resources :meetings
   resources :lists, only: %i[index show new create edit destroy] do
->>>>>>> master
     resources :tasks, only: %i[index show new edit update]
   end
 
