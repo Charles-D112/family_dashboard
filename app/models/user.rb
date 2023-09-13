043e814c8697b6187edcc99a8cff7a8a65a050d0
Charles Deotto <charles.deotto@gmail.com>
2023-09-13 16:28:35 +0200

    add has_many meetings dans user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :lists
+  has_many :meetings
   has_one_attached :photo
 
   def my_executed_tasks
